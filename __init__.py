e22e4d63465f4d87f6f78be2ac10342bb9dd7b30
tecnicounosuper <147665779+tecnicounosuper@users.noreply.github.com>
2026-05-04 07:08:10 +0200

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,18 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import uart
+from esphome.const import CONF_ID
+
+DEPENDENCIES = ['uart']
+
+seplos_ns = cg.esphome_ns.namespace('seplos_v3_p4')
+SeplosComponent = seplos_ns.class_('SeplosComponent', cg.Component, uart.UARTDevice)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(SeplosComponent),
+}).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
