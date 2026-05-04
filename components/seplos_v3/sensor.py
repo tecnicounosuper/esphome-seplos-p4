554be1a28a73333843cb124619d49449c6445a44
tecnicounosuper <147665779+tecnicounosuper@users.noreply.github.com>
2026-05-04 21:57:26 +0200

    Update sensor.py

--- components/seplos_v3/sensor.py
+++ components/seplos_v3/sensor.py
@@ -3,55 +3,46 @@ import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import (
     CONF_ID,
-    CONF_TYPE,
     CONF_ADDRESS,
+    CONF_TYPE,
+    ICON_BATTERY,
+    ICON_FLASH,
     UNIT_VOLT,
-    UNIT_AMPERE,
     UNIT_PERCENT,
-    DEVICE_CLASS_VOLTAGE,
-    DEVICE_CLASS_CURRENT,
-    DEVICE_CLASS_BATTERY,
-    STATE_CLASS_MEASUREMENT,
+    UNIT_AMPERE,
 )
 from . import seplos_v3_ns, SeplosComponent
 
-# Definizione dei tipi di sensore supportati
+DEPENDENCIES = ["seplos_v3"]
+
+# Tipi di sensori supportati basati sul manuale Seplos V3
 TYPES = {
     "battery_voltage": sensor.sensor_schema(
         unit_of_measurement=UNIT_VOLT,
+        icon=ICON_FLASH,
         accuracy_decimals=2,
-        device_class=DEVICE_CLASS_VOLTAGE,
-        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+    "battery_soc": sensor.sensor_schema(
+        unit_of_measurement=UNIT_PERCENT,
+        icon=ICON_BATTERY,
+        accuracy_decimals=1,
     ),
     "current": sensor.sensor_schema(
         unit_of_measurement=UNIT_AMPERE,
+        icon="mdi:lightning-bolt",
         accuracy_decimals=2,
-        device_class=DEVICE_CLASS_CURRENT,
-        state_class=STATE_CLASS_MEASUREMENT,
-    ),
-    "soc": sensor.sensor_schema(
-        unit_of_measurement=UNIT_PERCENT,
-        accuracy_decimals=1,
-        device_class=DEVICE_CLASS_BATTERY,
-        state_class=STATE_CLASS_MEASUREMENT,
     ),
 }
 
-CONF_SEPLOS_V3_ID = "seplos_v3_id"
-
-# CORREZIONE: Usiamo sensor.sensor_schema().extend(...) invece di sensor.SENSOR_SCHEMA
-CONFIG_SCHEMA = sensor.sensor_schema().extend({
-    cv.GenerateID(CONF_SEPLOS_V3_ID): cv.use_id(SeplosComponent),
-    cv.Required(CONF_ADDRESS): cv.int_range(min=0, max=15),
-    cv.Required(CONF_TYPE): cv.one_of(*TYPES, lower=True),
-})
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID("seplos_v3_id"): cv.use_id(SeplosComponent),
+        cv.Required(CONF_ADDRESS): cv.int_range(min=0, max=16),
+        cv.Required(CONF_TYPE): cv.one_of(*TYPES, lower=True),
+    }
+).extend(sensor.sensor_schema()).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
-    hub = await cg.get_variable(config[CONF_SEPLOS_V3_ID])
-    
-    # Crea l'oggetto sensore basandosi sul tipo scelto nel YAML
-    # Recuperiamo lo schema specifico dal dizionario TYPES
-    type_config = TYPES[config[CONF_TYPE]]
+    parent = await cg.get_variable(config["seplos_v3_id"])
     var = await sensor.new_sensor(config)
-    
-    cg.add(hub.register_sensor(config[CONF_ADDRESS], config[CONF_TYPE], var))
+    cg.add(parent.register_sensor(config[CONF_ADDRESS], config[CONF_TYPE], var))
