1d7b80c07c0f40a1247ffe69aa12ddc6b617ad16
tecnicounosuper <147665779+tecnicounosuper@users.noreply.github.com>
2026-05-15 21:58:14 +0200

    Update __init__.py

--- components/seplos_v3/__init__.py
+++ components/seplos_v3/__init__.py
@@ -4,15 +4,16 @@ from esphome.components import uart
 from esphome.const import CONF_ID
 
 DEPENDENCIES = ['uart']
+AUTO_LOAD = ['sensor', 'text_sensor']
 
 seplos_v3_ns = cg.esphome_ns.namespace('seplos_v3')
-SeplosComponent = seplos_v3_ns.class_('SeplosComponent', cg.Component, uart.UARTDevice)
+SeplosV3 = seplos_v3_ns.class_('SeplosV3', cg.PollingComponent, uart.UARTDevice)
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(SeplosComponent),
-}).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
+    cv.GenerateID(): cv.declare_id(SeplosV3),
+}).extend(cv.polling_component_schema('60s')).extend(uart.uart_device_schema(bauds=[19200]))
 
-async def to_code(config):
+def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await uart.register_uart_device(var, config)
+    yield cg.register_component(var, config)
+    yield uart.register_uart_device(var, config)
