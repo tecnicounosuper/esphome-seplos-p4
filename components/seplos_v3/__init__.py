66d5f845c984d652e1de083aaf1b89f4ccb81c5a
tecnicounosuper <147665779+tecnicounosuper@users.noreply.github.com>
2026-05-15 22:25:07 +0200

    Update __init__.py

--- components/seplos_v3/__init__.py
+++ components/seplos_v3/__init__.py
@@ -3,17 +3,19 @@ import esphome.config_validation as cv
 from esphome.components import uart
 from esphome.const import CONF_ID
 
+# Definiamo le dipendenze
 DEPENDENCIES = ['uart']
-AUTO_LOAD = ['sensor', 'text_sensor']
+AUTO_LOAD = ['sensor']
 
 seplos_v3_ns = cg.esphome_ns.namespace('seplos_v3')
 SeplosV3 = seplos_v3_ns.class_('SeplosV3', cg.PollingComponent, uart.UARTDevice)
 
+# La correzione principale è qui sotto
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(SeplosV3),
-}).extend(cv.polling_component_schema('60s')).extend(uart.uart_device_schema(bauds=[19200]))
+}).extend(cv.polling_component_schema('60s')).extend(uart.UART_DEVICE_SCHEMA)
 
-def to_code(config):
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
-    yield uart.register_uart_device(var, config)
+    await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
