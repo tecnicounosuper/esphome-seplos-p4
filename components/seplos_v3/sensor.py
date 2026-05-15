cd7de3a7e2b0a4860b9368022424a189e0663c14
tecnicounosuper <147665779+tecnicounosuper@users.noreply.github.com>
2026-05-15 22:19:02 +0200

    Update sensor.py

--- components/seplos_v3/sensor.py
+++ components/seplos_v3/sensor.py
@@ -10,16 +10,16 @@ from . import seplos_v3_ns, SeplosV3
 
 CONF_SEPLOS_V3_ID = "seplos_v3_id"
 
-# Qui definiamo i "TYPE" esatti da scrivere nello YAML
+# Qui definiamo i nomi ESATTI da usare nello YAML
 TYPES = {
     "battery_voltage": sensor.sensor_schema(unit_of_measurement=UNIT_VOLT, accuracy_decimals=2, device_class=DEVICE_CLASS_VOLTAGE),
+    "battery_soc": sensor.sensor_schema(unit_of_measurement=UNIT_PERCENT, accuracy_decimals=1, device_class=DEVICE_CLASS_BATTERY),
     "current": sensor.sensor_schema(unit_of_measurement=UNIT_AMPERE, accuracy_decimals=2, device_class=DEVICE_CLASS_CURRENT),
-    "soc": sensor.sensor_schema(unit_of_measurement=UNIT_PERCENT, accuracy_decimals=1, device_class=DEVICE_CLASS_BATTERY),
 }
 
-# Genera automaticamente cell_1, cell_2 ... cell_16
+# Aggiungiamo le 16 celle alla lista dei comandi validi
 for i in range(1, 17):
-    TYPES[f"cell_{i}"] = sensor.sensor_schema(unit_of_measurement=UNIT_VOLT, accuracy_decimals=3, device_class=DEVICE_CLASS_VOLTAGE)
+    TYPES[f"cell_{i}_voltage"] = sensor.sensor_schema(unit_of_measurement=UNIT_VOLT, accuracy_decimals=3, device_class=DEVICE_CLASS_VOLTAGE)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(CONF_SEPLOS_V3_ID): cv.use_id(SeplosV3),
