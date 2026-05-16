1799f9fa4f589ce35d7cfe2b541b5b7c56c2dda3
tecnicounosuper <147665779+tecnicounosuper@users.noreply.github.com>
2026-05-16 16:06:43 +0200

    Update sensor.py

--- components/seplos_v3/sensor.py
+++ components/seplos_v3/sensor.py
@@ -4,7 +4,7 @@ from esphome.components import sensor
 from esphome.const import (
     CONF_ID, CONF_TYPE, CONF_ADDRESS, 
     DEVICE_CLASS_VOLTAGE, DEVICE_CLASS_CURRENT, DEVICE_CLASS_BATTERY, DEVICE_CLASS_TEMPERATURE,
-    UNIT_VOLT, UNIT_AMPERE, UNIT_PERCENT, UNIT_CELSIUS
+    UNIT_VOLT, UNIT_AMPERE, UNIT_PERCENT, UNIT_CELSIUS, UNIT_AMPERE_HOUR
 )
 from . import seplos_v3_ns, SeplosV3
 
@@ -12,16 +12,19 @@ CONF_SEPLOS_V3_ID = "seplos_v3_id"
 
 TYPES = {
     "battery_voltage": sensor.sensor_schema(unit_of_measurement=UNIT_VOLT, accuracy_decimals=2, device_class=DEVICE_CLASS_VOLTAGE),
-    "battery_soc": sensor.sensor_schema(unit_of_measurement=UNIT_PERCENT, accuracy_decimals=1, device_class=DEVICE_CLASS_BATTERY),
     "current": sensor.sensor_schema(unit_of_measurement=UNIT_AMPERE, accuracy_decimals=2, device_class=DEVICE_CLASS_CURRENT),
+    "battery_soc": sensor.sensor_schema(unit_of_measurement=UNIT_PERCENT, accuracy_decimals=1, device_class=DEVICE_CLASS_BATTERY),
+    "remaining_capacity": sensor.sensor_schema(unit_of_measurement=UNIT_AMPERE_HOUR, accuracy_decimals=2, icon="mdi:battery-charging-100"),
+    "total_capacity": sensor.sensor_schema(unit_of_measurement=UNIT_AMPERE_HOUR, accuracy_decimals=2, icon="mdi:battery-100"),
 }
 
 # Genera automaticamente cell_1_voltage ... cell_16_voltage
 for i in range(1, 17):
     TYPES[f"cell_{i}_voltage"] = sensor.sensor_schema(unit_of_measurement=UNIT_VOLT, accuracy_decimals=3, device_class=DEVICE_CLASS_VOLTAGE)
 
-# Genera automaticamente temperature_1 ... temperature_4
-for i in range(1, 5):
+# Genera automaticamente temperature_1 ... temperature_4 (Sensori celle)
+# e temperature_5 (Temperatura ambiente/MOSFET)
+for i in range(1, 6):
     TYPES[f"temperature_{i}"] = sensor.sensor_schema(unit_of_measurement=UNIT_CELSIUS, accuracy_decimals=1, device_class=DEVICE_CLASS_TEMPERATURE)
 
 CONFIG_SCHEMA = cv.Schema({
