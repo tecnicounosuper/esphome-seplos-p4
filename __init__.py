4351e6db55b362463bd1957b3a1447df3e530822
tecnicounosuper <147665779+tecnicounosuper@users.noreply.github.com>
2026-05-16 23:18:32 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,13 +4,14 @@ from esphome.components import uart
 from esphome.const import CONF_ID
 
 DEPENDENCIES = ['uart']
+MULTI_CONF = True
 
-seplos_ns = cg.esphome_ns.namespace('seplos_v3_p4')
-SeplosComponent = seplos_ns.class_('SeplosComponent', cg.Component, uart.UARTDevice)
+seplos_parser_ns = cg.esphome_ns.namespace('seplos_parser')
+SeplosParser = seplos_parser_ns.class_('SeplosParser', cg.Component, uart.UARTDevice)
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(SeplosComponent),
-}).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
+    cv.GenerateID(): cv.declare_id(SeplosParser),
+}).extend(uart.UART_DEVICE_SCHEMA).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
