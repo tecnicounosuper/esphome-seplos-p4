80552362384130ca6806c8e350be0cff25946186
tecnicounosuper <147665779+tecnicounosuper@users.noreply.github.com>
2026-05-27 21:47:44 +0200

    Create binary_sensor.py

---
+++ components/seplos_parser/binary_sensor.py
@@ -0,0 +1,16 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import binary_sensor
+from . import SeplosParser
+
+CONF_SEPLOS_PARSER_ID = "seplos_parser_id"
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(CONF_SEPLOS_PARSER_ID): cv.use_id(SeplosParser),
+    }
+)
+
+async def to_code(config):
+    hub = await cg.get_variable(config[CONF_SEPLOS_PARSER_ID])
+    cg.add(hub.register_binary_sensor(hub))
