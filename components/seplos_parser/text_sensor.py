f383ebc49ea7ee81c5b91f9c0b63b711c57633c3
tecnicounosuper <147665779+tecnicounosuper@users.noreply.github.com>
2026-05-27 21:48:04 +0200

    Update text_sensor.py

--- components/seplos_parser/text_sensor.py
+++ components/seplos_parser/text_sensor.py
@@ -1,7 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import text_sensor
-from esphome.const import CONF_ID
 from . import SeplosParser
 
 CONF_SEPLOS_PARSER_ID = "seplos_parser_id"
