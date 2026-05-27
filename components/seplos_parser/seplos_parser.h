165434260cfa50b052d95b20d3d83ef0df79b865
tecnicounosuper <147665779+tecnicounosuper@users.noreply.github.com>
2026-05-27 21:12:03 +0200

    Update seplos_parser.h

--- components/seplos_parser/seplos_parser.h
+++ components/seplos_parser/seplos_parser.h
@@ -1,60 +1,43 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/core/defines.h"
 #include "esphome/components/uart/uart.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/text_sensor/text_sensor.h"
+#include "esphome/components/binary_sensor/binary_sensor.h"
 #include <vector>
 #include <deque>
-#include <cstdint>
-#include <cstddef>
 #include <string>
-
-#ifdef USE_SENSOR
-#include "esphome/components/sensor/sensor.h"
-#endif
-#ifdef USE_TEXT_SENSOR
-#include "esphome/components/text_sensor/text_sensor.h"
-#endif
 
 namespace esphome {
 namespace seplos_parser {
 
-class SeplosParser : public uart::UARTDevice, public Component {
-#ifdef USE_SENSOR
- protected:
-  std::vector<sensor::Sensor *> sensors_;
-
+class SeplosParser : public Component, public uart::UARTDevice {
  public:
-  void register_sensor(sensor::Sensor *obj) { this->sensors_.push_back(obj); }
-#endif
-
-#ifdef USE_TEXT_SENSOR
- protected:
-  std::vector<text_sensor::TextSensor *> text_sensors_;
-
- public:
-  void register_text_sensor(text_sensor::TextSensor *obj) { this->text_sensors_.push_back(obj); }
-#endif
-
-  void set_bms_count(int bms_count);
-  void set_update_interval(int update_interval);
-
   void setup() override;
   void loop() override;
   void dump_config() override;
 
+  void set_bms_count(int bms_count);
+  void set_update_interval(int update_interval);
+
+  // Registrazione dei sensori da ESPHome
+  void register_sensor(sensor::Sensor *s) { sensors_.push_back(s); }
+  void register_text_sensor(text_sensor::TextSensor *ts) { text_sensors_.push_back(ts); }
+  void register_binary_sensor(binary_sensor::BinarySensor *bs) { binary_sensors_.push_back(bs); }
+
  protected:
-  std::deque<uint8_t> buffer;
   int bms_count_{1};
-  uint32_t update_interval_{5000};
+  uint32_t update_interval_{10000}; // Default 10 secondi in millisecondi
+  std::deque<uint8_t> buffer;
   std::vector<uint32_t> last_updates_;
 
-  size_t get_expected_length();
-  bool validate_crc();
-  void process_packet();
-  bool should_update(int bms_index);
+  // Liste grezze collezionate dal costruttore ESPHome
+  std::vector<sensor::Sensor *> sensors_;
+  std::vector<text_sensor::TextSensor *> text_sensors_;
+  std::vector<binary_sensor::BinarySensor *> binary_sensors_;
 
-  // Vettori di sensori per memorizzare le istanze associate a ciascun BMS index
+  // Sensori Analogici Mappati per BMS (vettori indicizzati per bms_index)
   std::vector<sensor::Sensor *> pack_voltage_;
   std::vector<sensor::Sensor *> current_;
   std::vector<sensor::Sensor *> remaining_capacity_;
@@ -72,17 +55,29 @@ class SeplosParser : public uart::UARTDevice, public Component {
   std::vector<sensor::Sensor *> min_cell_temp_;
   std::vector<sensor::Sensor *> maxdiscurt_;
   std::vector<sensor::Sensor *> maxchgcurt_;
-  
-  // Celle (1-16) e Temperature (1-4) mappate dinamicamente
-  std::vector<std::vector<sensor::Sensor *>> cells_;
-  std::vector<std::vector<sensor::Sensor *>> temps_;
-  
   std::vector<sensor::Sensor *> case_temp_;
   std::vector<sensor::Sensor *> power_temp_;
-  
+
+  // Matrici per Celle e Temperature [Numero_Elemento][Indice_BMS]
+  std::vector<std::vector<sensor::Sensor *>> cells_;
+  std::vector<std::vector<sensor::Sensor *>> temps_;
+
+  // Sensori di Testo Mappati per BMS
   std::vector<text_sensor::TextSensor *> system_status_;
   std::vector<text_sensor::TextSensor *> active_alarm_;
 
+  // NUOVO: Sensori Binari MOSFET Mappati per BMS
+  std::vector<binary_sensor::BinarySensor *> chg_mos_status_;
+  std::vector<binary_sensor::BinarySensor *> dischg_mos_status_;
+
+  // NUOVO: Matrice Sensori Binari Bilanciamento [Cella_0_fino_15][Indice_BMS]
+  std::vector<std::vector<binary_sensor::BinarySensor *>> balancing_status_;
+
+  // Funzioni interne di parsing e instradamento dei pacchetti
+  size_t get_expected_length();
+  bool validate_crc();
+  void process_packet();
+  bool should_update(int bms_index);
   void map_sensor_vector(std::vector<sensor::Sensor *> &vec, const std::string &name);
 };
 
