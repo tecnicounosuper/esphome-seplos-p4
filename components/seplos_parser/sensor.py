43e1cb540247e2321918659f28ff57b7a986dbda
tecnicounosuper <147665779+tecnicounosuper@users.noreply.github.com>
2026-07-21 21:43:20 +0200

    Update sensor.py

--- components/seplos_parser/sensor.py
+++ components/seplos_parser/sensor.py
@@ -13,37 +13,28 @@ from esphome.const import (
     UNIT_PERCENT,
 )
 
-# Riferimenti all'Hub definiti in __init__.py
 from . import seplos_parser_ns, SeplosParser, CONF_SEPLOS_PARSER_ID, CONF_BMS_INDEX
 
-# Definizione della classe C++ del sensore
-SeplosSensor = seplos_parser_ns.class_("SeplosSensor", sensor.Sensor, cg.Component)
-
-# Schema dei tipi di sensore e delle loro unita' di misura
 SENSOR_TYPES = {
     "voltage": sensor.sensor_schema(
-        SeplosSensor,
         unit_of_measurement=UNIT_VOLT,
         accuracy_decimals=2,
         device_class=DEVICE_CLASS_VOLTAGE,
         state_class=STATE_CLASS_MEASUREMENT,
     ),
     "current": sensor.sensor_schema(
-        SeplosSensor,
         unit_of_measurement=UNIT_AMPERE,
         accuracy_decimals=2,
         device_class=DEVICE_CLASS_CURRENT,
         state_class=STATE_CLASS_MEASUREMENT,
     ),
     "soc": sensor.sensor_schema(
-        SeplosSensor,
         unit_of_measurement=UNIT_PERCENT,
         accuracy_decimals=0,
         device_class=DEVICE_CLASS_BATTERY,
         state_class=STATE_CLASS_MEASUREMENT,
     ),
     "capacity": sensor.sensor_schema(
-        SeplosSensor,
         unit_of_measurement="Ah",
         accuracy_decimals=2,
         state_class=STATE_CLASS_MEASUREMENT,
