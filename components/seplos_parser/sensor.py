0017ad2a200b4a6a1a5545443b3fba1421c984b9
tecnicounosuper <147665779+tecnicounosuper@users.noreply.github.com>
2026-07-21 21:08:20 +0200

    Update sensor.py

--- components/seplos_parser/sensor.py
+++ components/seplos_parser/sensor.py
@@ -1,18 +1,6 @@
-import esphome.codegen as cg
-import esphome.config_validation as cv
-from esphome.components import sensor
-from . import SeplosParser
-
-CONF_SEPLOS_PARSER_ID = "seplos_parser_id"
-
-CONFIG_SCHEMA = sensor.sensor_schema().extend(
-    {
-        cv.GenerateID(CONF_SEPLOS_PARSER_ID): cv.use_id(SeplosParser),
-    }
-)
-
-
-async def to_code(config):
-    hub = await cg.get_variable(config[CONF_SEPLOS_PARSER_ID])
-    sens = await sensor.new_sensor(config)
-    cg.add(hub.register_sensor(sens))
+sensor:
+  - platform: seplos_parser
+    seplos_parser_id: seplos_hub
+    bms_index: 0
+    type: voltage
+    name: "BMS 1 Tensione Totale"
