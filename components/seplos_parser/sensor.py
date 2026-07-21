f4f5216d4afda6f0b4d60bd3da286a4619195efa
tecnicounosuper <147665779+tecnicounosuper@users.noreply.github.com>
2026-07-21 21:29:06 +0200

    Update sensor.py

--- components/seplos_parser/sensor.py
+++ components/seplos_parser/sensor.py
@@ -13,33 +13,37 @@ from esphome.const import (
     UNIT_PERCENT,
 )
 
-# Riferimento al namespace e all'Hub definiti in __init__.py
+# Riferimenti all'Hub definiti in __init__.py
 from . import seplos_parser_ns, SeplosParser, CONF_SEPLOS_PARSER_ID, CONF_BMS_INDEX
 
-# Definizione del tipo C++ per il sensore
+# Definizione della classe C++ del sensore
 SeplosSensor = seplos_parser_ns.class_("SeplosSensor", sensor.Sensor, cg.Component)
 
-# Schema dei tipi di sensori supportati
+# Schema dei tipi di sensore e delle loro unita' di misura
 SENSOR_TYPES = {
     "voltage": sensor.sensor_schema(
+        SeplosSensor,
         unit_of_measurement=UNIT_VOLT,
         accuracy_decimals=2,
         device_class=DEVICE_CLASS_VOLTAGE,
         state_class=STATE_CLASS_MEASUREMENT,
     ),
     "current": sensor.sensor_schema(
+        SeplosSensor,
         unit_of_measurement=UNIT_AMPERE,
         accuracy_decimals=2,
         device_class=DEVICE_CLASS_CURRENT,
         state_class=STATE_CLASS_MEASUREMENT,
     ),
     "soc": sensor.sensor_schema(
+        SeplosSensor,
         unit_of_measurement=UNIT_PERCENT,
         accuracy_decimals=0,
         device_class=DEVICE_CLASS_BATTERY,
         state_class=STATE_CLASS_MEASUREMENT,
     ),
     "capacity": sensor.sensor_schema(
+        SeplosSensor,
         unit_of_measurement="Ah",
         accuracy_decimals=2,
         state_class=STATE_CLASS_MEASUREMENT,
@@ -47,20 +51,18 @@ SENSOR_TYPES = {
 }
 
 CONFIG_SCHEMA = cv.All(
-    sensor.sensor_schema(SeplosSensor)
+    cv.TypedSchema(SENSOR_TYPES, lower=True)
     .extend(
         {
             cv.GenerateID(CONF_SEPLOS_PARSER_ID): cv.use_id(SeplosParser),
             cv.Required(CONF_BMS_INDEX): cv.int_range(min=0, max=15),
-            cv.Required(CONF_TYPE): cv.enum(SENSOR_TYPES, lower=True),
         }
     )
     .extend(cv.COMPONENT_SCHEMA)
 )
 
 async def to_code(config):
-    var = cg.new_variable(config[CONF_ID], config[CONF_TYPE])
-    await sensor.register_sensor(var, config)
+    var = await sensor.new_sensor(config)
     await cg.register_component(var, config)
 
     hub = await cg.get_variable(config[CONF_SEPLOS_PARSER_ID])
