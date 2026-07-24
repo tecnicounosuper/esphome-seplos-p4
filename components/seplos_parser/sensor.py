495810465d0eae546b35ef735490ac6a6fe86db0
tecnicounosuper <147665779+tecnicounosuper@users.noreply.github.com>
2026-07-24 23:07:34 +0200

    Update sensor.py

--- components/seplos_parser/sensor.py
+++ components/seplos_parser/sensor.py
@@ -2,16 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import (
-    CONF_ID,
-    CONF_NAME,
     CONF_TYPE,
-    CONF_UNIT_OF_MEASUREMENT,
-    CONF_DEVICE_CLASS,
-    CONF_STATE_CLASS,
-    CONF_ACCURACY_DECIMALS,
-    DEVICE_CLASS_VOLTAGE,
-    DEVICE_CLASS_CURRENT,
-    DEVICE_CLASS_BATTERY,
     STATE_CLASS_MEASUREMENT,
 )
 from . import seplos_parser_ns, SeplosParserHub
@@ -19,32 +10,31 @@ from . import seplos_parser_ns, SeplosParserHub
 CONF_SEPLOS_PARSER_ID = "seplos_parser_id"
 CONF_BMS_INDEX = "bms_index"
 
-SeplosSensor = seplos_parser_ns.class_("SeplosSensor", sensor.Sensor)
+TYPES = {
+    "pack_voltage": "set_pack_voltage_sensor",
+    "current": "set_current_sensor",
+    "soc": "set_soc_sensor",
+    "remaining_capacity": "set_remaining_capacity_sensor",
+}
 
-TYPES = [
-    "pack_voltage",
-    "current",
-    "soc",
-    "remaining_capacity",
-]
-
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(SeplosSensor),
-        cv.Required(CONF_SEPLOS_PARSER_ID): cv.use_id(SeplosParserHub),
-        cv.Required(CONF_BMS_INDEX): cv.int_range(min=0, max=15),
-        cv.Required(CONF_TYPE): cv.one_of(*TYPES, lower=True),
-    }
-).extend(sensor.sensor_schema(
-    accuracy_decimals=2,
-    state_class=STATE_CLASS_MEASUREMENT,
-))
+CONFIG_SCHEMA = (
+    sensor.sensor_schema(
+        accuracy_decimals=2,
+        state_class=STATE_CLASS_MEASUREMENT,
+    )
+    .extend(
+        {
+            cv.GenerateID(CONF_SEPLOS_PARSER_ID): cv.use_id(SeplosParserHub),
+            cv.Required(CONF_BMS_INDEX): cv.int_range(min=0, max=15),
+            cv.Required(CONF_TYPE): cv.one_of(*TYPES, lower=True),
+        }
+    )
+)
 
 async def to_code(config):
     hub = await cg.get_variable(config[CONF_SEPLOS_PARSER_ID])
-    var = cg.new_Pvariable(config[CONF_ID])
-    await sensor.register_sensor(var, config)
+    var = await sensor.new_sensor(config)
     
-    cg.add(var.set_bms_index(config[CONF_BMS_INDEX]))
-    cg.add(var.set_sensor_type(config[CONF_TYPE]))
-    cg.add(hub.register_sensor(var))
+    setter_name = TYPES[config[CONF_TYPE]]
+    setter = getattr(hub, setter_name)
+    cg.add(setter(config[CONF_BMS_INDEX], var))
