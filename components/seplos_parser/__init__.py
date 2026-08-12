a04e0389a5bf79a00839c86536e79236215fff8b
tecnicounosuper <147665779+tecnicounosuper@users.noreply.github.com>
2026-08-12 18:07:32 +0200

    Update __init__.py

--- components/seplos_parser/__init__.py
+++ components/seplos_parser/__init__.py
@@ -10,11 +10,13 @@ seplos_parser_ns = cg.esphome_ns.namespace("seplos_parser")
 SeplosParserHub = seplos_parser_ns.class_("SeplosParserHub", cg.PollingComponent, uart.UARTDevice)
 
 CONF_BMS_COUNT = "bms_count"
+CONF_ACTIVE_POLLING = "active_polling"
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(SeplosParserHub),
         cv.Optional(CONF_BMS_COUNT, default=2): cv.int_range(min=1, max=16),
+        cv.Optional(CONF_ACTIVE_POLLING, default=True): cv.boolean,
     }
 ).extend(cv.polling_component_schema("10s")).extend(uart.UART_DEVICE_SCHEMA)
 
@@ -23,3 +25,4 @@ async def to_code(config):
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
     cg.add(var.set_bms_count(config[CONF_BMS_COUNT]))
+    cg.add(var.set_active_polling(config[CONF_ACTIVE_POLLING]))
